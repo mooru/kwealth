7cce0b1d30b446dfb0e3ae5fa4fabc4401dd6904
Martin Oru <martinoru@gmail.com>
2013-05-28 22:29:25 +0100

    More changes

--- sites/all/themes/frodo/js/scriptos.js
+++ sites/all/themes/frodo/js/scriptos.js
@@ -2,6 +2,12 @@ jQuery(function () {
   jQuery('.node-26 img').eq(0).add('.node-27 img').attr('src' , "/kwealth/sites/default/files/foopdf.png");
   jQuery('.node-26 img').eq(1).attr('src' , "/kwealth/sites/default/files/64xls.png");
   jQuery('.node-26 img').eq(2).attr('src' , "/kwealth/sites/default/files/64xls.png");
-  jQuery('<img src="/kwealth/sites/default/files/Contact.png" />')
+  jQuery('<img src="/sites/default/files/Contact.png" />')
   .insertBefore('#contact-site-form div:first');
+
+jQuery('.page-admin-people #content #user-admin-account div table tr:last').empty();
+jQuery('.page-admin-people #content h1').text('Cooperative Members');
+jQuery('#user-admin-account table thead tr th a').attr('href', '#');
+
 });
+
